085215f8b20e6058300811987cbd21fe84ece5d9
rakhalib14 <rakhalib14@gmail.com>
2019-11-28 17:55:39 +0600

    3 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,20 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    id: 1
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+     id: 2
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+     id: 3
   }
 ];
 
